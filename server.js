ef673af5af2f1f774a74a84a0f2598551047a2fe
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-22 19:40:28 +0000

    Fulfill https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/registration-of-new-users

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ myDB(async client => {
   passport.use(new LocalStrategy(
     (username, password, done) => {
       myDataBase.findOne({ username: username }, (err, user) => {
-        console.log('User '+ username +' attempted to log in.');
+        console.log('User ' + username + ' attempted to log in.');
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
         if (password !== user.password) { return done(null, false); }
@@ -65,7 +65,8 @@ myDB(async client => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -79,54 +80,76 @@ myDB(async client => {
     });
   });
 
+  app.route('/register')
+     .post((req, res, next) => {
+       if (!req.body.username) {
+         res.redirect('/');
+       }
+       myDataBase.findOne({ username: req.body.username }, (err, user) => {
+         if (err) {
+           next(err);
+         } else if (user) {
+           res.redirect('/');
+         } else {
+           myDataBase.insertOne({
+             username: req.body.username,
+             password: req.body.password
+           }, (err, doc) => {
+             if (err) {
+               res.redirect('/');
+             } else {
+               console.log(doc.username + ' is successfully registered.')
+               // The inserted document is held within
+               // the ops property of the doc
+               next(null, doc.ops[0]);
+             }
+           })
+         }
+       })
+     },
+       passport.authenticate('local', { failureRedirect: '/' }),
+       (req, res) => {
+         res.redirect('/profile');
+       }
+     );
+
+  app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
+
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  };
+
+  app.get('/profile', ensureAuthenticated, (req, res) => {
+    res.render(process.cwd() + '/views/pug/profile', {
+      username: req.user.username
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
 
-app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-  res.redirect('/profile');
-});
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-})
-
-// -------------------------------------------------------------------
-// 
-
-const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
-};
-
-app.get('/profile', ensureAuthenticated, (req, res) => {
-  res.render(process.cwd() + '/views/pug/profile', {
-    username: req.user.username
-  });
-});
-
 
 // ===================================================================
 // 
-
-// https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/logging-a-user-out
-// app.use((req, res, next) => {
-//   res.status(404)
-//     .type('text')
-//     .send('Not Found');
-// });
-
-app.all(/^\/.+/, (req, res) => {
-  // console.log(`req`, req); // DEBUG
-  res.status(404)
-     .type('text')
-     .send('Not Found');
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
