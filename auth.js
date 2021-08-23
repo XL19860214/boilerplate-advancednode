fd045c65a4bb6c1c3044a59e5b85f991d53b4186
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-23 11:11:51 +0000

    Fulfill https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/implementation-of-social-authentication

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const GitHubStrategy = require('passport-github2');
 const bcrypt = require('bcrypt');
 
 const auth = (app, myDataBase) => {
@@ -19,7 +20,33 @@ const auth = (app, myDataBase) => {
     }
   ));
 
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: 'https://boilerplate-advancednode.xl19860214.repl.co/auth/github/callback'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // console.log(`profile`, profile); // DEBUG
+    myDataBase.findOne({ username: `github|${profile.id}` }, (err, user) => {
+      if (err || user) {
+        return done(err, user);
+      } else {
+        myDataBase.insertOne({
+          username: `github|${profile.id}`
+        }, (err, doc) => {
+          if (!err) {
+            console.log(doc.ops[0].username + ' is successfully registered throught GitHub.')
+            return done(null, doc.ops[0]);
+          }
+          return done(null, false);
+        })
+      }
+    });
+  }
+));
+
   passport.serializeUser((user, done) => {
+    // console.log(`passport.serializeUser::user`, user); // DEBUG
     done(null, user._id);
   });
 
@@ -28,6 +55,8 @@ const auth = (app, myDataBase) => {
       done(null, doc);
     });
   });
+
+
 }
 
 module.exports = auth;
