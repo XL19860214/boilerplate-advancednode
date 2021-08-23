a53282f5261cbd034be8d8acea9b48a8f8e4e42a
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-23 13:01:35 +0000

    Fulfill https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/authentication-with-socket-io

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const session = require('express-session');
 //
 const routes = require('./routes.js');
 const auth = require('./auth.js');
-
+//
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 // ===================================================================
 //
@@ -16,6 +21,31 @@ const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+const onAuthorizeSuccess = (data, accept) => {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+const onAuthorizeFail = (data, message, error, accept) => {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+
 
 let currentUsers = 0;
 
@@ -35,6 +65,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
+  key: 'express.sid',
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
